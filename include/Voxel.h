3b6833b438f3c44d06b521ebc4c09986abb6f143
Jason Addison <28279470+Voxol@users.noreply.github.com>
2020-07-20 01:06:43 -0400

    Add files via upload

---
+++ include/Voxel.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "Vec3f.h"
+#include <vector>
+
+class Voxel
+{
+public:
+	Voxel();
+	~Voxel();
+
+	Vec3f average = Vec3f(0, 0, 0);
+	Vec3f smoothAverage = Vec3f(0, 0, 0);
+	float count;
+	Vec3f fn = Vec3f(0, 0, 0);
+	bool flipX = false;
+	bool flipY = false;
+	bool flipZ = false;
+	bool avgCalc = false;
+	int total = 0;
+
+	Vec3f material;
+	int materialCounter = 0;
+	int mat = 0;
+
+	std::vector<short> totalMaterial;
+	std::vector<short> totalMaterialCounter;
+
+	float x = 0;
+	float y = 0;
+	float z = 0;
+
+	Vec3f getAverage();
+
+	void addAverage(Vec3f avg);
+
+	void addMaterial(int m);
+
+	void findMostCommonMaterial();
+
+	short getCommonMaterial();
+};
+
